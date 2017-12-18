094894854932277f1e9f081206c1d4e6aab0b206
KellyStamps <kellydsample@gmail.com>
2017-12-18 18:23:28 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with MyValidator
+
+  validates :title, presence: true
+
+  validates :content, length: { minimum: 250 }
+
+  validates :summary, length: { maximum: 250 }
+
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid size" }
+
+  validates_with :title, MyValidator
+
+  # validates :size, inclusion: { in: %w(small medium large),
+  #   message: "%{value} is not a valid size" }
+
+end
+
+
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    record.title.include? 'Won\'t Believe Secret Top [number] Guess'
+  end
 end
